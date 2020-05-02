f48bb9a4ae893b5c6ab801c54142eb348e66b408
abdrahmandiab <37490334+abdrahmandiab@users.noreply.github.com>
2020-05-02 09:44:43 +0200

    Added Front end skeleton with React

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>This is a full MERN stack app for Keeping contacts</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default Home;
