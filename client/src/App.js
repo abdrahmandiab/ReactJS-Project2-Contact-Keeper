5c260203eb712c0747522955c00c6021685ab046
abdrahmandiab <37490334+abdrahmandiab@users.noreply.github.com>
2020-05-03 21:59:00 +0200

    Private Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import Navbar from "./components/layout/Navbar";
 import Alerts from "./components/layout/Alerts";
 import Home from "./components/pages/Home";
@@ -8,6 +9,7 @@ import About from "./components/pages/About";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import ContactState from "./context/contact/ContactState";
+
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
 import setAuthToken from "./utils/setAuthToken";
@@ -27,7 +29,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
