a49d99e54472dfd981ee78d73e2afcb471b079e5
abdrahmandiab <37490334+abdrahmandiab@users.noreply.github.com>
2020-05-04 00:24:21 +0200

    Deploy #3

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// When using Node, we cannot use import unless we use TypeScript or Babble etc.
-// Otherwise we have to use this syntax which is called Common JS
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
@@ -12,19 +10,15 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API..." })
-);
-
-//Define Routes
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
 // Serve static assets in production
-if ((process.env.NODE_ENV = "production")) {
-  //Set static folder
-  app.use(express.static("cleint/build"));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
