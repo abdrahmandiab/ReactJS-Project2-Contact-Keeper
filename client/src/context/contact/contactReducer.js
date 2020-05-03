b8ae543f07d78a21ed37f8ce1b64183bdcee3ad6
abdrahmandiab <37490334+abdrahmandiab@users.noreply.github.com>
2020-05-03 05:22:27 +0200

    Added Contact list ,addition, and remove functionalities

---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,29 @@
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_ALERT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+export default (state, action) => {
+  console.log(action.payload);
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id != action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+};
