177c3f767a98e6deb3a73d3821e63722e8fb8b6b
abdrahmandiab <37490334+abdrahmandiab@users.noreply.github.com>
2020-05-03 23:57:17 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // Otherwise we have to use this syntax which is called Common JS
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -11,14 +12,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API..." })
-);
-
 //Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if ((process.env.NODE_ENV = "production")) {
+  //Set static folder
+  app.use(express.static("cleint/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
